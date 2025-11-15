3c14ec03d00897c8f888f3c22457d1758c08c401
Ivan Davidov <ivandav067@gmail.com>
2025-11-15 10:34:56 -0600

    Made ParkOnlyAutoOp work without odometers/encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoOpPrograms/ParkOnlyAutoOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoOpPrograms/ParkOnlyAutoOp.java
@@ -22,10 +22,19 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.hardware.Motor;
+import org.firstinspires.ftc.teamcode.myAuto.Practice1;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
 @Autonomous(name = "Parking Only Autonomous", group = "Autonomous")
 public class ParkOnlyAutoOp extends OpMode {
+
+    public enum States
+    {
+        INIT,
+        FIRST_MOVE,
+        SECOND_MOVE,
+        END
+    }
 
     //Movement Motors
     Motor motorFL;
@@ -33,23 +42,13 @@ public class ParkOnlyAutoOp extends OpMode {
     Motor motorBL;
     Motor motorBR;
 
-    //Pedro Pathing Instance Variables
-    private Follower follower;
-
-    //These values can be adjusted later depending on time
-    private Pose startPose = new Pose(56,8); //red
-    //private Pose startPose = new Pose(88, 8); //blue
-
-    private Pose connection = new Pose(76,46);
-
-    private Pose parkPose = new Pose(38.718, 33.934); //red
-    //private Pose parkPose = (105.282, 33.934); //blue
-
-    private Path parkPath;
-
     //Sets up Timer
+    private static final double MOVE_TIME = 0.5;
     private Timer autoTimer;
+    private Timer moveTimer;
     private static double MAX_TIME = 25.0;
+
+    States state;
 
 
     /**Initializes the motors, follower, path, and timer
@@ -63,13 +62,10 @@ public class ParkOnlyAutoOp extends OpMode {
         this.motorBL = new Motor(hardwareMap,"motorBL");
         this.motorBR = new Motor(hardwareMap,"motorBR");
 
-        this.follower = Constants.createFollower(hardwareMap);
-        buildTheParkingPath();
-        this.follower.setStartingPose(startPose);
-
+        moveTimer = new Timer();
         autoTimer = new Timer();
 
-
+        state = States.INIT;
 
     }
 
@@ -80,7 +76,7 @@ public class ParkOnlyAutoOp extends OpMode {
     public void start()
     {
         autoTimer.resetTimer();
-
+        moveTimer.resetTimer();
     }
 
     /**Continues this code until time either runs out or the robot is in position
@@ -89,28 +85,59 @@ public class ParkOnlyAutoOp extends OpMode {
     @Override
     public void loop()
     {
-        if(autoTimer.getElapsedTimeSeconds() >= MAX_TIME){
-            motorFL.setPower(0);
-            motorFR.setPower(0);
-            motorBL.setPower(0);
-            motorBR.setPower(0);
-        }
-        if(follower.isBusy()){
-            follower.update();
-        }
-        else{
-            motorFL.setPower(0);
-            motorFR.setPower(0);
-            motorBL.setPower(0);
-            motorBR.setPower(0);
+        switch(state)
+        {
+            case INIT:
+                state = States.FIRST_MOVE;
+                break;
+            case FIRST_MOVE:
+                moveTimer.resetTimer();
+                motorFL.setPower(1);
+                motorFR.setPower(1);
+                motorBL.setPower(1);
+                motorBR.setPower(1);
+
+                if (moveTimer.getElapsedTimeSeconds() >= 1)
+                {
+                    state = States.SECOND_MOVE;
+                }
+                else if (autoTimer.getElapsedTimeSeconds() >= MOVE_TIME * 1.5)
+                {
+                    state = States.END;
+                }
+
+                break;
+            case SECOND_MOVE:
+                moveTimer.resetTimer();
+                motorFL.setPower(1);
+                motorFR.setPower(-1);
+                motorBL.setPower(-1);
+                motorBR.setPower(1);
+
+                if (moveTimer.getElapsedTimeSeconds() >= MOVE_TIME || autoTimer.getElapsedTimeSeconds() >= MAX_TIME)
+                {
+                    state = States.END;
+                }
+
+                break;
+            case END:
+                motorFL.setPower(0);
+                motorFR.setPower(0);
+                motorBL.setPower(0);
+                motorBR.setPower(0);
+                break;
+            default:
+                System.out.println("Undefined state reached");
+                motorFL.setPower(0);
+                motorFR.setPower(0);
+                motorBL.setPower(0);
+                motorBR.setPower(0);
+                break;
         }
     }
 
     /**Builds the parking path based on values found
      *
      */
-    private void buildTheParkingPath(){
-        parkPath = new Path(new BezierCurve(startPose, connection, parkPose));
-        parkPath.setLinearHeadingInterpolation(0, NINETY);
-    }
+
 }
