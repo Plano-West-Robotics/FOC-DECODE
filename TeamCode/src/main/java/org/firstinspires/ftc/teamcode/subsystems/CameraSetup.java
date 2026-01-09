e2783d0b9ba9a46527c0b2fdbec28aab5ff4cd63
aryahigairola <aryahiga@gmail.com>
2026-01-08 22:34:00 -0600

    Added controls for theoretical flywheel system. Commented out ServoFlap for temporary testing Replaced Servo code with CRServo Code Forward/Backward only back wheels move (strafe stays the same otherwise) Finished the autonomous for parking Updated DriverControlled for strafing triggered by right and left triggers. Modified Teleop to be based off of Game Manual 0 for more accuracy Created a new Launcher subsystem (LauncherTwo) to align with the updated engineering design, and implemented it into the teleOp Updated intake, outtake, and transfer values Fixed some timer logic issues with the ParkingAutoOp Created a camera subsystem that sets up the camera to integrate with autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/CameraSetup.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+public class CameraSetup {
+
+    private VisionPortal visPort;
+    private AprilTagProcessor atp;
+
+    public static final double FX = 8.245077776187888503e2;
+    public static final double FY = 8.270054620332543891e2;
+    public static final double CX = 3.212181651421647643e2;
+    public static final double CY = 2.425865530229235674e2;
+
+    public static final int TAG_BLUE = 20;
+    public static final int TAG_RED = 24;
+
+    private double xOff = 0;
+    private double yOff = 0;
+    private double dist = 0;
+
+    private boolean hasTag = false;
+
+    public CameraSetup(HardwareMap hardwaremap){
+
+        atp = new AprilTagProcessor.Builder().setLensIntrinsics(FX, FY, CX, CY).setDrawAxes(false).setDrawCubeProjection(false).setDrawTagOutline(false).build();
+
+        visPort = new VisionPortal.Builder().setCamera(hardwaremap.get(WebcamName.class, "Webcam")).setCameraResolution(new Size(640, 480)).addProcessor(atp).build();
+
+    }
+
+    public void update(){
+        List<AprilTagDetection> detections = atp.getDetections();
+
+        hasTag = false;
+
+        for (AprilTagDetection tag: detections){
+            if(tag.id == TAG_BLUE || tag.id == TAG_RED){
+                xOff = tag.ftcPose.x;
+                yOff = tag.ftcPose.y;
+                dist = tag.ftcPose.range;
+                hasTag = true;
+                return;
+            }
+        }
+
+
+    }
+
+    public boolean hasTagCheck(){
+        return hasTag;
+    }
+
+    public double getXOff(){
+        return xOff;
+    }
+
+    public double getYOff(){
+        return yOff;
+    }
+
+    public double getDist(){
+        return dist;
+    }
+
+    public void stop(){
+        visPort.close();
+    }
+
+
+
+}
